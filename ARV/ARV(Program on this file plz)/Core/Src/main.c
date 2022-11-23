baff7709a8d319af2aa66a610a443db992c1a3fa
yanjerrrrr <hychandb@connect.ust.hk>
2022-11-23 14:12:58 +0800

    trying to test the servo degree by button

--- ARV/ARV(Program on this file plz)/Core/Src/main.c
+++ ARV/ARV(Program on this file plz)/Core/Src/main.c
@@ -38,7 +38,49 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+	typedef enum {
+	    BOTH_BTN, BTN1_ONLY, BTN2_ONLY, ALL_OFF,
+	} ButtonState;
 
+	typedef enum {
+	    BUTTON1, BUTTON2,
+
+	    //This trick means the NumButtons will equal how many buttons there are (how many enum values)
+	    //This works as long as the enum values are simple (count up from 0)
+	    NUM_BUTTON,
+	} Button;
+
+	typedef enum {
+	    LED1, LED2, LED3, LED4, NUM_LED,
+	} LED;
+
+	/**
+	 * @brief read the button state
+	 * return 1 if the button is pressed;
+	 * return 0 if the button is released;
+	 */
+	static inline uint8_t read_button(Button btn) {
+	    switch (btn) {
+	    case BUTTON1:
+	        return !btn_read(BTN1);
+	    case BUTTON2:
+	        return !btn_read(BTN2);
+	    default:
+	        return 0;
+	    }
+	}
+
+	static ButtonState btn_state(void) {
+	    if (read_button(BUTTON1) && read_button(BUTTON2)) {
+	        return BOTH_BTN;
+	    } else if (read_button(BUTTON1)) {
+	        return BTN1_ONLY;
+	    } else if (read_button(BUTTON2)) {
+	        return BTN2_ONLY;
+	    } else {
+	        return ALL_OFF;
+	    }
+	}
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -269,6 +311,10 @@ int main(void)
   		//Motors
   		TIM10->CCR1 = 839;
   		TIM11->CCR1 = 839;
+
+  		if (btn_state == ALL_OFF){
+  			gpio_toggle(LED1);
+  		}
   		forward();
 
   	}
